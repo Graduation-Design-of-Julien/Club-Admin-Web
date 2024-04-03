1d23ebab3e3ba5cb9c2855f532a3f7570599a262
JulienZeng <936636589@qq.com>
2024-04-03 22:00:54 +0800

    路由添加layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,12 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 import ResetPwd from "../pages/ResetPwd.vue";
 import LoginPage from "../pages/LoginPage.vue";
-
+import LayoutPage from "../pages/layout/index.vue"
 
 const routes: Array<RouteRecordRaw> = [
     { path: "/login", component: LoginPage },
     { path: "/reset", component: ResetPwd },
+    { path: "/layout", component: LayoutPage },
     // { path: '/about', component: About },
 ];
 
