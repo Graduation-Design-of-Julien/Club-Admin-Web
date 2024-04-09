2a943ab5f700ab0f820739acfdab83d21091d77b
JulienZeng <936636589@qq.com>
2024-04-10 01:08:25 +0800

    add autoimport config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import IconsResolver from 'unplugin-icons/resolver'
+import IconsResolver from "unplugin-icons/resolver";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         AutoImport({
+            eslintrc: {
+                enabled: true,
+            },
             resolvers: [
                 ElementPlusResolver(),
                 IconsResolver({
