b7ec4ebe8877628ec3d271fba69dba84f3e7ecb5
JulienZeng <936636589@qq.com>
2024-05-12 03:03:16 +0800

    完成footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        style="
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;
+            height: 100%;
+        "
+    >
+        <div>© JulienZeng 2024</div>
+
+        <div>
+            <el-link href="https://beian.miit.gov.cn/" target="_blank"
+                >粤ICP备2022014957号-1</el-link
+            >
+        </div>
+
+        <div
+            style="
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+            "
+        >
+            <el-image
+                style="width: 20px; height: 20px"
+                src="https://img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png"
+            />
+            <el-link
+                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44049102496850/"
+                target="_blank"
+                >粤公网安备 44049102496850号</el-link
+            >
+        </div>
+    </div>
+</template>
