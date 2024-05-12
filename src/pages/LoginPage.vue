fa4198379dec82e974933f5b391307324515e0c7
JulienZeng <77975074+JulienZeng@users.noreply.github.com>
2024-05-12 10:37:03 +0800

    add 备案

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -74,6 +74,25 @@
                 </el-tab-pane>
             </el-tabs>
         </el-card>
+            <div>
+        <br /><br />
+        <el-link href="https://beian.miit.gov.cn/" target="_blank"
+            >粤ICP备2022014957号-1</el-link
+        >
+        <div
+            style="display: flex; flex-direction: row; justify-content: center"
+        >
+            <el-image
+                style="width: 20px; height: 20px"
+                src="https://img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png"
+            />
+            <el-link
+                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44049102496850/"
+                target="_blank"
+                >粤公网安备 44049102496850号</el-link
+            >
+        </div>
+    </div>
     </div>
 </template>
 
